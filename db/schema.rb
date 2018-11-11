fa8ad21d2c45b709af23b76d5c019324015c3bd4
solliegarcia <sollie@hawaii.edu>
2018-11-10 19:02:54 -1000

    Update database

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_11_11_032137) do
+ActiveRecord::Schema.define(version: 2018_11_11_045858) do
 
   create_table "dishes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -32,6 +31,9 @@ ActiveRecord::Schema.define(version: 2018_11_11_032137) do
     t.text "list_of_dishes"
     t.string "name"
     t.integer "user_id"
+    t.string "course"
+    t.datetime "date"
+    t.string "location"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
