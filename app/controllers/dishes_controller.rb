6065649457cd5cce800eb923ea307255791bb3f2
garyjw <31455599+garyjw@users.noreply.github.com>
2018-12-03 23:30:02 -1000

    dishes-controller-edit

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,7 +1,24 @@
 class DishesController < ApplicationController
   def create
+  	@dish = Dish.new(dish_params)
+    if @dish.save
+      #log_in(@user)
+      flash[:success] = "Welcome to Manoa Meals"
+      redirect_to user_path(@user)
+    else
+      flash.now[:danger] = "Error creating account"
+      render 'new'
+    end
   end
 
   def new
+  	@dish = Dish.new
   end
+
+private
+
+	def dish_params
+      params.require(:dish).permit(:username, :email_address, :first_name, :last_name)
+    end
+
 end
